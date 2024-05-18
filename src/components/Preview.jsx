2ace853a2a3c7afe82834e17749c1a9b24abeef3
Daniel Kai <danielkai@DanieldeMacBook-Air.local>
2024-05-19 00:51:26 +0800

    3

--- src/components/Preview.jsx
+++ src/components/Preview.jsx
@@ -1,37 +1,38 @@
 import React, { useEffect } from 'react';
 
-function Preview({ imageSrc, printPhoto, croppedImageSrc, setCroppedImageSrc, handleRetry,IP}) {
+function Preview({ imageSrc, printPhoto, croppedImageSrc, setCroppedImageSrc, handleRetry, IP }) {
 
   useEffect(() => {
     if (imageSrc) {
       const image = new Image();
       image.src = imageSrc;
       image.onload = () => {
+        // 提高解析度的倍數
+        const scaleFactor = 2; // 這裡設置為2，意味著解析度為原來的2倍
+
         const canvas = document.createElement('canvas');
         const ctx = canvas.getContext('2d');
         const targetAspectRatio = 8 / 9;
         let width, height;
 
         if (image.width / image.height > targetAspectRatio) {
-          height = image.height;
+          height = image.height * scaleFactor;
           width = height * targetAspectRatio;
         } else {
-          width = image.width;
+          width = image.width * scaleFactor;
           height = width / targetAspectRatio;
         }
 
         canvas.width = width;
         canvas.height = height;
-        const xOffset = (image.width - width) / 2;
-        const yOffset = (image.height - height) / 2;
+        const xOffset = (image.width * scaleFactor - width) / 2;
+        const yOffset = (image.height * scaleFactor - height) / 2;
 
-        ctx.drawImage(image, xOffset, yOffset, width, height, 0, 0, width, height);
+        ctx.drawImage(image, xOffset / scaleFactor, yOffset / scaleFactor, width / scaleFactor, height / scaleFactor, 0, 0, width, height);
         setCroppedImageSrc(canvas.toDataURL('image/png'));
       };
     }
   }, [imageSrc, setCroppedImageSrc]);
-
-  
 
   return (
     <article className='preview'>
